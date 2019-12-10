02c550cb1c8248ba032ee86395b889608a8daafd
Damian <>
2019-12-10 01:38:56 +0100

    Attempted to create the skeleton. currently trying to find out how to create references in the editor. We can look at the Input manager and unitys "shop system" they should be doing something we are looking for.

---
+++ Packages/GameplayFramework/Editor/GameExplorer/GameExplorer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using GameplayFramework.Core;
+using System.IO;
+
+namespace GameplayFramework.Editor
+{
+    public class GameExplorer : EditorWindow
+    {
+
+        static GameExplorer editorInstance;
+
+        [MenuItem("GameplayFramework/GameExplorer")]
+        public static void Open()
+        {
+            if (editorInstance == null)
+            {
+                editorInstance = EditorWindow.GetWindow<GameExplorer>();
+                editorInstance.titleContent = new GUIContent("GameExplorer");
+            }
+
+            GameSettings settings = FindObjectOfType<GameSettings>();
+            if(settings == null)
+            {
+                settings = Resources.Load<GameSettings>("GameSettings");
+
+            }
+
+            
+
+            //in case a user deleted the settings object
+            if (settings == null)
+            {
+                Debug.LogWarning("Created new instance of gameSettings");
+                settings = CreateInstance<GameSettings>();
+                
+                if (!AssetDatabase.IsValidFolder("Assets/Resources"))
+                {
+                    AssetDatabase.CreateFolder("Assets", "Resources");
+                }
+
+                //TODO create this inside Packages/GameplayFramework/GameSettings.asset instead
+                AssetDatabase.CreateAsset(settings, "Assets/Resources/GameSettings.asset");
+            }
+
+            Debug.Log($"pre:{settings.gamemode != null}");
+            settings.gamemode = new GamemodeBase();
+
+
+            Debug.Log($"preLUL:{settings.a}");
+            settings.a = "ree";
+            Debug.Log($"postLUL:{settings.a}");
+            Debug.Log($"post:{settings.gamemode != null}");
+            EditorUtility.SetDirty(settings);
+
+            AssetDatabase.SaveAssets();
+
+            editorInstance.Show();
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("test");
+
+        }
+    }
+}
---
+++ Packages/GameplayFramework/Runtime/Core/Game/GameSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+
+namespace GameplayFramework.Core
+{
+    public class GameSettings : ScriptableObject , ISerializationCallbackReceiver
+    {
+        [SerializeField]
+        public GamemodeBase gamemode;
+
+        private string GameModeClass = "";
+
+        [SerializeField]
+        public string a = "lul";
+
+        public void OnAfterDeserialize()
+        {
+            Debug.Log($"gamemode full name :{GameModeClass}");
+        }
+
+        public void OnBeforeSerialize()
+        {
+            GameModeClass = gamemode.GetType().AssemblyQualifiedName;
+        }
+    }
+}
